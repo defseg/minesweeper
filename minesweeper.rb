690c9be12b6779ddbc0f150a82e2e761893cccf0
defseg <6988161+defseg@users.noreply.github.com>
2014-11-17 11:12:20 -0500

    Initialize repository

---
+++ minesweeper.rb
@@ -0,0 +1,17 @@
+class Minesweeper
+  def initialize
+    @BOMBS = 10
+    @X_DIM = 9
+    @Y_DIM = 9
+    @grid = Array.new(@Y_DIM) { Array.new(@X_DIM) }
+
+  end
+
+  private
+
+    def seed_grid
+      
+
+
+    end
+end

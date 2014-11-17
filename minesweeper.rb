770ae4b7e7024d5e834f7ae31571b5595641b580
defseg <6988161+defseg@users.noreply.github.com>
2014-11-17 11:21:49 -0500

    Add seed_grid

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,5 +1,6 @@
 class Minesweeper
-  def initialize
+
+    def initialize
     @BOMBS = 10
     @X_DIM = 9
     @Y_DIM = 9
@@ -7,11 +8,21 @@ class Minesweeper
 
   end
 
-  private
+  # private
 
     def seed_grid
-      
+      @BOMBS.times do
+        bomb_placed = false
+        until bomb_placed
+          y_loc = rand(@Y_DIM)
+          x_loc = rand(@X_DIM)
+          unless @grid[y_loc][x_loc]
+            @grid[y_loc][x_loc] = :bomb
+            bomb_placed = true
+          end
+        end
+      end
 
-
+      @grid
     end
 end

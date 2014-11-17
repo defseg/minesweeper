d0f741f1f53f14591c935e8d73778c334478c315
defseg <6988161+defseg@users.noreply.github.com>
2014-11-17 11:31:20 -0500

    Add get_user_input

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,14 +1,43 @@
 class Minesweeper
 
     def initialize
-    @BOMBS = 10
-    @X_DIM = 9
-    @Y_DIM = 9
-    @grid = Array.new(@Y_DIM) { Array.new(@X_DIM) }
+      @BOMBS = 10
+      @X_DIM = 9
+      @Y_DIM = 9
+      @grid = Array.new(@Y_DIM) { Array.new(@X_DIM) }
+      seed_grid
+    end
 
-  end
+    def get_user_input
+      puts "Pick a square. Input your choice in the format row,column."
+      puts "Prefix your choice with r to reveal or f to flag."
 
-  # private
+      #r3,7 or f0,0 etc.
+
+      user_input = gets.chomp
+
+      # a[0] should be r or f
+      # split(',') everything else
+
+      if a[0] = 'r'
+        user_action = :reveal
+      elsif a[0] = 'f'
+        user_action = :flag
+      else
+        raise "Invalid input"
+      end
+
+      # TODO add error-checking to make sure the coords are valid
+      user_coordinates = a[1..-1].split(',').map(&:to_i)
+
+      [user_action, user_coordinates]
+    end
+
+    def check_user_input
+
+    end
+
+  private
 
     def seed_grid
       @BOMBS.times do

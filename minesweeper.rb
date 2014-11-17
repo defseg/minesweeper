ae3134698ded0dfda525624e328234ffb179c241
defseg <6988161+defseg@users.noreply.github.com>
2014-11-17 12:35:18 -0500

    More refactoring

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,12 +1,15 @@
 class Tile
 
-  attr_accessor :revealed, :number, :flagged, :bombed
+  attr_accessor :number, :flagged, :bombed
 
-  def initialize(bombed)
-    @revealed = false
+  def initialize
     @number = nil
     @flagged = false
-    @bombed = bombed
+    @bombed = false
+  end
+
+  def revealed?
+    !!@number   
   end
 
   def flag
@@ -18,60 +21,29 @@ end
 class Board
 
   def initialize
-
+    @BOMBS = 10
+    @X_DIM = 9
+    @Y_DIM = 9
+    @grid = Array.new(@Y_DIM) { Array.new(@X_DIM) { Tile.new } }
+    seed_grid
   end
 
-end
-
-class Minesweeper
-
-    def initialize
-      @BOMBS = 10
-      @X_DIM = 9
-      @Y_DIM = 9
-      @grid = Array.new(@Y_DIM) { Array.new(@X_DIM) { [nil, false]} }
-      seed_grid
+  def flag_tile(y, x)
+    if @grid[y][x].number == nil || @grid[y][x].bombed
+      @grid[y][x].flagged = true
+    else
+      raise "Can't flag a numbered square"
     end
+  end
 
-    def get_user_input
-      puts "Pick a square. Input your choice in the format row,column."
-      puts "Prefix your choice with r to reveal or f to flag."
-      #r3,7 or f0,0 etc.
-
-      user_input = gets.chomp
-
-      if user_input[0] = 'r'
-        user_action = :reveal
-      elsif user_input[0] = 'f'
-        user_action = :flag
-      else
-        raise "Invalid input"
-      end
-
-      # TODO add error-checking to make sure the coords are valid
-      user_coordinates = user_input[1..-1].split(',').map(&:to_i)
-
-      [user_action, user_coordinates]
-    end
-
-    def execute_user_action(user_action, user_coordinates) # auto-decompose array
-      user_y, user_x = user_coordinates
-      if user_action == :flag
-        if @grid[user_y][user_x][0] == nil || @grid[user_y][user_x][0] == :bomb
-          @grid[user_y][user_x][1] = true
-        else
-          raise "Can't flag a numbered square"
-        end
-      elsif user_action == :reveal
-        reveal_square(user_y, user_x)
-      end
-    end
+  def reveal_square(y, x)
+    # check if it's already been revealed
+    # check if it's a bomb
+    # if not, get its number
+    # if it's zero, go through each adjacent tile and reveal_square it recursively
+  end
 
   private
-
-    def reveal_square(y, x)
-
-    end
 
     def seed_grid
       @BOMBS.times do
@@ -79,13 +51,53 @@ class Minesweeper
         until bomb_placed
           y_loc = rand(@Y_DIM)
           x_loc = rand(@X_DIM)
-          unless @grid[y_loc][x_loc][0]
-            @grid[y_loc][x_loc][0] = :bomb
+          unless @grid[y_loc][x_loc].bombed
+            @grid[y_loc][x_loc].bombed = true
             bomb_placed = true
           end
         end
       end
-
-      @grid
     end
+
+    def get_adjacent_squares(y, x)
+
+    end
+
+end
+
+class Minesweeper
+
+  def initialize
+    @board = Board.new
+  end
+
+  def get_user_input
+    puts "Pick a square. Input your choice in the format row,column."
+    puts "Prefix your choice with r to reveal or f to flag."
+    #r3,7 or f0,0 etc.
+
+    user_input = gets.chomp
+
+    if user_input[0] = 'r'
+      user_action = :reveal
+    elsif user_input[0] = 'f'
+      user_action = :flag
+    else
+      raise "Invalid input"
+    end
+
+    # TODO add error-checking to make sure the coords are valid
+    user_coordinates = user_input[1..-1].split(',').map(&:to_i)
+
+    [user_action, user_coordinates]
+  end
+
+  def execute_user_action(user_action, user_coordinates) # auto-decompose array
+    user_y, user_x = user_coordinates
+    if user_action == :flag
+      @board.flag(user_y, user_x)
+    elsif user_action == :reveal
+      @board.reveal_square(user_y, user_x)
+    end
+  end
 end
